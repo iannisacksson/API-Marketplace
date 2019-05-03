5b0e75febf3b9a518460bd127df9df859e1e29a2
iann <iisackssonoliveira@gmail.com>
2019-05-03 20:08:38 -0300

    Criptografia de senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -19,6 +20,15 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+})
+
+// Quero que algum aconteça antes de uma ação, neste caso antes do save
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  this.password = await bcrypt.hash(this.password, 8)
 })
 
 module.exports = mongoose.model('User', UserSchema)
